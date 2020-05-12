11dcb42e99e3b38633d2c8953e6dd0cf85f1f06a
Leonardo Guilen <leonardoguilen1@gmail.com>
2020-05-12 18:54:48 -0300

    Correção da url da api que retorna os estados e cidades

--- Backend/src/api/rootObject/services/rootObject.service.ts
+++ Backend/src/api/rootObject/services/rootObject.service.ts
@@ -11,70 +11,82 @@ dotenv.config({
 
 @injectable()
 export class RootObjectService implements IRootObjectService {
-    private _basePlacesUrl: URL
-    private _baseWeatherUrl: URL
+  private _basePlacesUrl: URL;
+  private _baseWeatherUrl: URL;
+  private readonly _geoId: string = '3469034';
 
-    public constructor () {
-      this._basePlacesUrl = new URL('http://www.geonames.org/childrenJSON?geonameId=3469034&callback=listPlaces&style=long&noCacheIE=1585428772700');
-      this._baseWeatherUrl = new URL(`https://api.openweathermap.org/data/2.5/weather?q={city name}&units=metric&appid=${process.env.API_KEY}`);
-    }
+  public constructor() {
+    this._basePlacesUrl = new URL(
+      `http://www.geonames.org/childrenJSON?geonameId=${this._geoId}&callback=listPlaces&style=long&noCacheIE=1585428772700`
+    );
+    this._baseWeatherUrl = new URL(
+      `https://api.openweathermap.org/data/2.5/weather?q={city name}&units=metric&appid=${process.env.API_KEY}`
+    );
+  }
 
-    async GetListRootObjectModelStateAsync (): Promise<IRootObjectModel[]> {
-      const rootObj = async (): Promise<IRootObjectModel[]> => {
-        try {
-          const response = await fetch(this._basePlacesUrl);
-          const json: string = await (await response.text())
-            .replace(/listPlaces\({"totalResultsCount":\d{0,5},"geonames":/g, '')
-            .replace('});', '');
+  async GetListRootObjectModelStateAsync(): Promise<IRootObjectModel[]> {
+    const rootObj = async (): Promise<IRootObjectModel[]> => {
+      try {
+        const response = await fetch(this._basePlacesUrl);
+        const json: string = await (await response.text())
+          .replace(/listPlaces\({"totalResultsCount":\d{0,5},"geonames":/g, '')
+          .replace('});', '');
 
-          const objList: IRootObjectModel[] = JSON.parse(json);
+        const objList: IRootObjectModel[] = JSON.parse(json);
 
-          return objList;
-        } catch (error) {
-          throw Error(error);
-        }
-      };
+        return objList;
+      } catch (error) {
+        throw Error(error);
+      }
+    };
 
-      return await rootObj();
-    }
+    return await rootObj();
+  }
 
-    async GetListRootObjectModelCityAsync (stateGeoId: string): Promise<IRootObjectModel[]> {
-      this._basePlacesUrl.searchParams.set('geonameId', stateGeoId);
+  async GetListRootObjectModelCityAsync(
+    stateGeoId: string
+  ): Promise<IRootObjectModel[]> {
+    this._basePlacesUrl.searchParams.set('geonameId', stateGeoId);
 
-      const rootObj = async (): Promise<IRootObjectModel[]> => {
-        try {
-          const response = await fetch(this._basePlacesUrl);
-          const json: string = await (await response.text())
-            .replace(/listPlaces\({"totalResultsCount":\d{0,5},"geonames":/g, '')
-            .replace('});', '');
+    const rootObj = async (): Promise<IRootObjectModel[]> => {
+      try {
+        const response = await fetch(this._basePlacesUrl);
+        const json: string = await (await response.text())
+          .replace(/listPlaces\({"totalResultsCount":\d{0,5},"geonames":/g, '')
+          .replace('});', '');
 
-          const objList: IRootObjectModel[] = JSON.parse(json);
+        const objList: IRootObjectModel[] = JSON.parse(json);
 
-          return objList;
-        } catch (error) {
-          throw Error(error);
-        }
-      };
+        return objList;
+      } catch (error) {
+        throw Error(error);
+      } finally {
+        this._basePlacesUrl.searchParams
+          .set('geonameId', this._geoId);
+      }
+    };
 
-      return await rootObj();
-    }
+    return await rootObj();
+  }
 
-    async GetRootObjectWeatherAsync (placeName: string): Promise<IRootObjectWeatherModel> {
-      this._baseWeatherUrl.searchParams.set('q', placeName);
+  async GetRootObjectWeatherAsync(
+    placeName: string
+  ): Promise<IRootObjectWeatherModel> {
+    this._baseWeatherUrl.searchParams.set('q', placeName);
 
-      const rootWeatherObj = async (): Promise<IRootObjectWeatherModel> => {
-        try {
-          const response = await fetch(this._baseWeatherUrl);
-          const json: string = await (await response.text());
+    const rootWeatherObj = async (): Promise<IRootObjectWeatherModel> => {
+      try {
+        const response = await fetch(this._baseWeatherUrl);
+        const json: string = await await response.text();
 
-          const objWeather: IRootObjectWeatherModel = JSON.parse(json);
+        const objWeather: IRootObjectWeatherModel = JSON.parse(json);
 
-          return objWeather;
-        } catch (error) {
-          throw Error(error);
-        }
-      };
+        return objWeather;
+      } catch (error) {
+        throw Error(error);
+      }
+    };
 
-      return await rootWeatherObj();
-    }
+    return await rootWeatherObj();
+  }
 }
