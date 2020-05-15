02bb02fe1c6fcb7379fac5e38b7edc947eabc363
Leonardo Guilen <leonardoguilen1@gmail.com>
2020-05-14 21:37:45 -0300

    Configuração dos arquivos estaticos para apontar no diretório da aplicação

--- Frontend/vue.config.js
+++ Frontend/vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/vue-node-weather-app/'
+        : '/',
     chainWebpack: (config) => {
         config.plugin("html").tap((args) => {
             args[0].title = "Weather App";
